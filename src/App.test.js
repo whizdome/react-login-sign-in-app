235c995ff1cea79874b6a790dfdecdcab74f8201
whizdome <wisdomolaniyan16@gmail.com>
2024-07-12 13:31:09 +0100

    rect App.test.js update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from 'react';
 
 test('renders learn react link', () => {
-  render(<App />);
+  // Render the App component wrapped in act
+  act(() => {
+    render(<App />);
+  });
+
+  // Log the HTML content for debugging purposes
+  console.log(document.body.innerHTML);
+
+  // Search for the link element with the text "learn react"
   const linkElement = screen.getByText(/learn react/i);
+  
+  // Check if the element is in the document
   expect(linkElement).toBeInTheDocument();
 });
